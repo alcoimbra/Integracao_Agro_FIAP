4c8fdafe8c4f51349e93fe6e2dbffac647e83301
Alan Coimbra <algcoimbra01@gmail.com>
2022-08-05 16:20:26 -0300

    Initial Structure Project

---
+++ agro-consumer/src/test/java/br/com/agroconsumer/AgroConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agroconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ agro-producer/src/test/java/br/com/agroproducer/AgroProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agroproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
